11af29b69a90b86aa455bb0dfe262891e7ce4255
Baha Karakaya <bhkrky@gmail.com>
2024-10-26 16:11:04 +0300

    integration

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import numpy as np
 import cv2
+from PIL import Image
+
+from util import get_limits
 
 cap = cv2.VideoCapture(0)
+yellow = [0, 255, 255]
 while True:
     ret, frame = cap.read()
+
+    lowerLimit, upperLimit = get_limits(color=yellow)
+
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
+
+    mask_ = Image.fromarray(mask)
+    bbox = mask_.getbbox()
+
+    if bbox:
+        x1, y1, x2, y2 = bbox
+
+        cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 5)
 
     cv2.imshow('frame', frame)
 
--- util.py
+++ util.py
@@ -6,8 +6,8 @@ def get_limits(color):
     c = np.uint8([[color]]) #bgr values which you want to convert to hsvv
     hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
 
-    lowerLimit = hsvC[0][0][0] - 10, 100, 100
-    upperLimit = hsvC[0][0][0] + 10, 255, 255
+    lowerLimit = hsvC[0][0][0] - 6, 100, 100
+    upperLimit = hsvC[0][0][0] + 6, 255, 255
 
     lowerLimit = np.array(lowerLimit, dtype=np.uint8)
     upperLimit = np.array(upperLimit, dtype=np.uint8)
